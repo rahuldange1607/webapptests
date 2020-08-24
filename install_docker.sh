59fe701d0f3899ed92b1dfaaf2d5768acbff0fdb
Rahul Dange <rahul@admarvel.com>
2020-08-24 19:31:12 +0530

    fixed typo in install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -4,4 +4,4 @@ echo "Installing puppet module for docker"
 /opt/puppetlabs/bin/puppet module install puppetlabs-docker --version 3.11.0
 
 echo "Installing docker using puppet module"
-/opt/puppetlabs/bin/puppet apply ./puppet/manifests/include_docker.pp
+/opt/puppetlabs/bin/puppet apply ./puppet/manifests/install_docker.pp
