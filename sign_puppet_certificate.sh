bd790789e7dccb8489f767a1f07846ad39ba4f7e
Rahul Dange <rahul@admarvel.com>
2020-08-24 18:02:20 +0530

    updated logic to detect if certificates are waiting to be signed

--- sign_puppet_certificate.sh
+++ sign_puppet_certificate.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 echo "Listing all certificates"
-/opt/puppetlabs/bin/puppetserver ca list > ca_list
-ca_list_content=$(cat ca_list.txt)
-echo $ca_list_content
+ca_list=$(/opt/puppetlabs/bin/puppetserver ca list)
+echo $ca_list
 
-if [ "$ca_list_content" != "No certificates to list" ]; then
+if [ "$ca_list" != "No certificates to list" ]; then
 	echo "Signing all certificates"
 	/opt/puppetlabs/bin/puppetserver ca sign --all
 else
