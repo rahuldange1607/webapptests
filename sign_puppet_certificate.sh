78fd5a18ac60ba862604ed1c9216c182d9f623ab
Rahul Dange <rahul@admarvel.com>
2020-08-24 17:51:06 +0530

    fixed error in case no certificates to sign

--- sign_puppet_certificate.sh
+++ sign_puppet_certificate.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 echo "Listing all certificates"
-/opt/puppetlabs/bin/puppetserver ca list
+/opt/puppetlabs/bin/puppetserver ca list > ca_list
+ca_list_content = $(cat my_file.txt)
+echo $ca_list_content
 
-echo "Signing all certificates"
-/opt/puppetlabs/bin/puppetserver ca sign --all
+if [ "$ca_list_content" != "No certificates to list" ]; then
+	echo "Signing all certificates"
+	/opt/puppetlabs/bin/puppetserver ca sign --all
+else
+	echo "No certificates to sign"
+fi
