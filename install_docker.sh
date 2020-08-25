0f3e0c13ddc832892dbb71c0f349c083e9261fa4
Rahul Dange <rahul@admarvel.com>
2020-08-25 10:31:13 +0530

    skip uploading puppet module to forge

--- install_docker.sh
+++ install_docker.sh
@@ -6,11 +6,8 @@ echo "Building the puppet module to install docker"
 rm -rf pkg
 pdk build --force
 
-echo "Uploading the puppet module to puppet forge"
-pdk release --force --version 0.1.0
-
 echo "Installing puppet module"
-/opt/puppetlabs/bin/puppet module install install_docker_ce --version 0.1.0
+/opt/puppetlabs/bin/puppet module install pkg/edureka-install_docker_ce-0.1.0.tar.gz --force --ignore-dependencies
 
 echo "Adding puppet module to the default puppet manifest"
 grep -qxF 'install_docker_ce' /etc/puppetlabs/code/environments/production/manifests/site.pp || echo 'class {''install_docker_ce'':}' >> /etc/puppetlabs/code/environments/production/manifests/site.pp
