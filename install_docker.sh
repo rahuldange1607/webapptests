209ef30bb3803a6f72d2c360d9d4bad27ef87a73
Rahul Dange <rahul@admarvel.com>
2020-08-25 14:14:43 +0530

    fixed issue in updating default puppet manifest

--- install_docker.sh
+++ install_docker.sh
@@ -11,10 +11,13 @@ echo "Installing puppet module install_docker_ce"
 /opt/puppetlabs/bin/puppet module install pkg/edureka-install_docker_ce-0.1.0.tar.gz --force --ignore-dependencies
 
 echo "Adding puppet module install_docker_ce to the default puppet manifest"
-grep -qxF 'install_docker_ce' /etc/puppetlabs/code/environments/production/manifests/site.pp || echo 'class {''install_docker_ce'':}' >> /etc/puppetlabs/code/environments/production/manifests/site.pp
+grep -qF 'install_docker_ce' /etc/puppetlabs/code/environments/production/manifests/site.pp || echo 'class {''install_docker_ce'':}' >> /etc/puppetlabs/code/environments/production/manifests/site.pp
 
 echo "Installing puppet module maven"
 /opt/puppetlabs/bin/puppet module install maestrodev-maven --version 1.4.0
 
 echo "Adding puppet module maven to the default puppet manifest"
-grep -qxF 'maven::maven' /etc/puppetlabs/code/environments/production/manifests/site.pp || echo 'class { ''maven::maven'':version => ''3.6.3'',}' >> /etc/puppetlabs/code/environments/production/manifests/site.pp
+grep -qF 'maven::maven' /etc/puppetlabs/code/environments/production/manifests/site.pp || echo 'class { ''maven::maven'':version => ''3.6.3''}' >> /etc/puppetlabs/code/environments/production/manifests/site.pp
+
+class {install_docker_ce:}
+class { maven::maven:version => 3.6.3,}
