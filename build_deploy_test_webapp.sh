370b976a69e024588832b795c60d12b8cf3e8106
Rahul Dange <rahul@admarvel.com>
2020-08-25 14:03:10 +0530

    update puppet agent before building docker image

--- build_deploy_test_webapp.sh
+++ build_deploy_test_webapp.sh
@@ -2,11 +2,14 @@
 
 BUILD_NUMBER=$1
 
+echo "Updating puppet agent"
+/opt/puppetlabs/bin/puppet agent --test
+
 echo "Building docker image for webapp"
 docker build -t rahuldange1607/webapp:${BUILD_NUMBER} .
 
 echo "Deploying webapp docker container"
-docker run -d -p 80:80 rahuldange1607/webapp:${BUILD_NUMBER} --name webapp
+docker run -d -p 80:80 --name webapp rahuldange1607/webapp:${BUILD_NUMBER}
 
 echo "Building selenium tests for webapp"
 mvn clean install
