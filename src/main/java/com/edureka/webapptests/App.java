df11339631ca124f90145b3536da91d2416fb5ca
Rahul Dange <rahul@admarvel.com>
2020-08-25 19:48:23 +0530

    added additional tests to check if Products and Contact links are working.

--- src/main/java/com/edureka/webapptests/App.java
+++ src/main/java/com/edureka/webapptests/App.java
@@ -19,10 +19,25 @@ public class App {
     	ChromeOptions chromeOptions = new ChromeOptions();
     	chromeOptions.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
     	WebDriver driver = new ChromeDriver(chromeOptions);
+    	
     	driver.get("http://localhost/index.php");
     	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     	driver.findElement(By.id("About Us")).click();
     	Assert.assertEquals(driver.findElement(By.id("PID-ab2-pg")).isDisplayed(), true);
+    	System.out.println("About Us link is working");
+    	
+    	driver.get("http://localhost/index.php");
+    	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    	driver.findElement(By.id("Products")).click();
+    	Assert.assertEquals(driver.findElement(By.id("PID-p4d-pg")).isDisplayed(), true);
+    	System.out.println("Products link is working");
+    	
+    	driver.get("http://localhost/index.php");
+    	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    	driver.findElement(By.id("Contact")).click();
+    	Assert.assertEquals(driver.findElement(By.id("PID-c92-pg")).isDisplayed(), true);
+    	System.out.println("Contact link is working");
+    	
     	driver.quit();
     }
 
