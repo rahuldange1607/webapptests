d632a46aad28c018a0d38348e97e7d8c11fb2019
Rahul Dange <rahul@admarvel.com>
2020-08-25 18:27:48 +0530

    fixed maven issue

--- build_deploy_test_webapp.sh
+++ build_deploy_test_webapp.sh
@@ -9,10 +9,11 @@ echo "Deploying webapp docker container"
 docker run -d -p 80:80 --name webapp rahuldange1607/webapp:${BUILD_NUMBER}
 
 echo "Building selenium tests for webapp"
-mvn clean install
+mvn clean package
 
 echo "Testing webapp"
 java -cp target/webapptests-0.0.1-SNAPSHOT.jar com.edureka.webapptests.App
 
 echo "Stopping webapp docker container"
 docker container stop webapp
+docker container rm $(docker container ls -aq)
